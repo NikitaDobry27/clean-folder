149b22a5710ad5d63790d623df4d1b269e18df86
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-03-07 21:13:45 +0100

    Folder name bug fixed

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_namespace_packages
 
-
 setup(
     name='clean-folder',
-    version='1.0',
-    description='Sorts files in the folder',
-    url='https://github.com/NikitaDobry27/clean-folder',
-    author='Nikita Bazhenov',
+    version='1',
+    description='Very useful code',
+    url='http://github.com/dummy_user/useful',
+    author='NikitaDobry27',
     author_email='bazhenov.nikita27@gmail.com',
+    license='MIT',
     packages=find_namespace_packages(),
     install_requires=['markdown'],
-    entry_points={'console_scripts': ['clean-folder = clean-folder.sort:final_sort']}
+    entry_points={'console_scripts': ['clean-folder = clean_folder.sort:final_sort']}
 )
