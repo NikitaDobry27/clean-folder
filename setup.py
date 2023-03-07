cc243cbc49dcd51e2f06065b4fe5c9b0b5ac972e
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-03-07 17:51:34 +0100

    License removed

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     name='clean-folder',
     version='1.0',
     description='Sorts files in the folder',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/NikitaDobry27/clean-folder',
     author='Nikita Bazhenov',
     author_email='bazhenov.nikita27@gmail.com',
-    license='MIT',
     packages=find_namespace_packages(),
     install_requires=['markdown'],
     entry_points={'console_scripts': ['clean-folder = clean-folder.sort:final_sort']}
